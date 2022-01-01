587864d81cd31e75d6bc0a4221c7bd7876227d59
Ertuğrul Demir <priest0758smiley@hotmail.com>
2022-01-01 22:31:48 +0300

    www

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,61 +15,29 @@ namespace ConsoleUI
             BrandManager brandManager = new BrandManager(new EfBrandDal());
             ColorManager colorManager = new ColorManager(new EfColorDal());
 
-            Car car1 = new Car { CarId=1,BrandId=1,ColorId=3,CarName="X",
+            Car car1 = new Car { CarId=2,BrandId=1,ColorId=3,CarName="Tesla",
                 ModelYear=new DateTime(2022,1,1),DailyPrice=55200,Descriptions="Tesla X"
             };
-            Color color = new Color {ColorId=1,ColorName="Red"};
-            Brand brand = new Brand {BrandId=1,BrandName="Tesla"};
 
             List<Car> cars = new List<Car> { 
-                new Car {CarId=4,BrandId=4,ColorId=4,CarName="Tesla",
+                new Car {CarId=1,BrandId=1,ColorId=1,CarName="Tesla",
                     ModelYear=new DateTime(2022,1,1),DailyPrice=50000,Descriptions="Tesla S"},
-                new Car {CarId=5,BrandId=5,ColorId=5,CarName="Tesla",
+                new Car {CarId=2,BrandId=1,ColorId=2,CarName="Tesla",
                     ModelYear=new DateTime(2012,1,1),DailyPrice=150000,Descriptions="Tesla 3"},
-                new Car {CarId=6,BrandId=6,ColorId=6,CarName="BMW",
+                new Car {CarId=3,BrandId=2,ColorId=1,CarName="BMW",
                     ModelYear=new DateTime(2020,1,1),DailyPrice=40000,Descriptions="BWM X"},
-                new Car {CarId=7,BrandId=7,ColorId=7,CarName="Porsche",
+                new Car {CarId=4,BrandId=4,ColorId=3,CarName="Porsche",
                     ModelYear=new DateTime(2015,1,1),DailyPrice=75000,Descriptions="Porsche A"},
-                new Car {CarId=8,BrandId=8,ColorId=8,CarName="Ferrari",
+                new Car {CarId=5,BrandId=3,ColorId=4,CarName="Ferrari",
                     ModelYear=new DateTime(2017,1,1),DailyPrice=125000,Descriptions="Ferrari L"},
             };
+            carManager.Add(car1);
 
-            List<Brand> brands = new List<Brand> { 
-                new Brand {BrandId=1,BrandName="Tesla"},
-                new Brand {BrandId=2,BrandName="BWM"},
-                new Brand {BrandId=3,BrandName="Ferrari"},
-                new Brand {BrandId=4,BrandName="Porsche"},
-            };
-
-            List<Color> colors = new List<Color> { 
-                new Color{ColorId=1,ColorName="Red"},
-                new Color{ColorId=2,ColorName="White"},
-                new Color{ColorId=3,ColorName="Black"},
-                new Color{ColorId=4,ColorName="Blue"},
-            };
-
-            foreach (var car in carManager.GetAll())
-            {
-                //Console.WriteLine(car.Descriptions);
-
-            }
-
-            //carManager.Add(new Car
-            //{
-            //    CarId = 7,
-            //    BrandId = 7,
-            //    ColorId = 7,
-            //    CarName = "Xaaaaaaaa",
-            //    ModelYear = new DateTime(2015, 1, 1),
-            //    DailyPrice = 175000,
-            //    Descriptions = "X A"
-            //});
             Console.Write("---------------");
-            foreach(var car in carManager.GetCarsByColorId(8))
+            foreach(var car in carManager.GetCarsByColorId(3))
             {
                 Console.WriteLine(car.Descriptions);
             }
-
 
         }
     }
